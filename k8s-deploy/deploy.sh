7aa857e251dd4ba561f252a5e207c75015ac3568
Ádám Sándor <10675791+adam-sandor@users.noreply.github.com>
2023-05-11 14:24:01 +0200

    remove pk from public file

--- k8s-deploy/deploy.sh
+++ k8s-deploy/deploy.sh
@@ -6,8 +6,9 @@ kubectl create ns $K8S_NAMESPACE
 kubectl ns $K8S_NAMESPACE
 kubectl label namespaces $K8S_NAMESPACE istio-injection=enabled
 
+kubectl create secret generic gcs-credentials --from-literal=pk="$(cat gcs-pk.txt)" -n $K8S_NAMESPACE
 kubectl create configmap opa-config --from-file=k8s-deploy/opa-conf.yaml -n $K8S_NAMESPACE
 
 kubectl apply -f k8s-deploy/istio-gateway.yaml
 kubectl apply -f k8s-deploy/envoy-filter.yaml
-kubectl apply -k k8s-deploy/services
+kubectl apply -k k8s-deploy/with-opa
